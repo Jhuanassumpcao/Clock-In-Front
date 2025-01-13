0941e22af14537a6d7e33264b2b674bc5cefeaa5
jhuanassumpcao <jlassumpcao@inf.ufsm.br>
2025-01-12 21:50:18 -0300

    fix: add UseContext

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import { UserProvider } from "./contexts/UserContext";
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <UserProvider>
+      <App />
+    </UserProvider>
+</React.StrictMode>,
 );
